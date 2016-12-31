3bd5271b49e206ce487ffc84eaf6d70be1a1fb43
ilya <holtzilya2008@gmail.com>
2016-12-31 06:03:29 +0200

    added logging feature to cleanproc

--- cleanproc/checkproc.sh
+++ cleanproc/checkproc.sh
@@ -1,14 +1,21 @@
 #! /bin/bash
 
-whitelist_path="... add our path wor the whitelist ..."
+whitelist="...your whitelist full path and filename..."
+logfile="...your log file full path and filename..."
 echo "WARNING: The following processes are not in your whitelist:"
 while read line
 do
-  found=$(grep $line $whitelist_path/whitelist.txt)
-  if  [[ "$found" == "" ]]; then
-    	echo $line
+  found=$(grep $line $whitelist)
+  if [[ "$found" == "" ]]; then
+		echo $line
+		logged=$(grep $line $logfile)
+	    if [[ "$logged" == "" ]]; then
+			echo $line >> $logfile
+			echo "Writing to" $logfile
+	    fi 
   fi 
+
 done
 
 exit 0
-       
+	   
