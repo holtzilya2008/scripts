e2e2a37f794c3f0bf1cce27d64cb9da7d5a65ee4
Ilya Holtz <holtzilya2008@gmail.com>
2023-04-22 14:11:42 +0300

    added archive subdirs script

---
+++ fileWork/archive_subdirs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check for the correct number of arguments
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+  echo "Usage: $0 [-gz] <path>"
+  exit 1
+fi
+
+# Check if the optional -gz flag is provided
+if [ "$1" == "-gz" ]; then
+  compress_gz=true
+  shift
+else
+  compress_gz=false
+fi
+
+path="$1"
+
+if [ ! -d "$path" ]; then
+  echo "Error: $path is not a valid directory"
+  exit 1
+fi
+
+# Loop through the subdirectories and create tar or tar.gz files
+for subdir in "$path"/*/; do
+  if [ -d "$subdir" ]; then
+    base_name="$(basename "$subdir")"
+    if $compress_gz; then
+      tar -cvzf "$path/$base_name.tar.gz" -C "$path" "$base_name" --gzip --best
+    else
+      tar -cvf "$path/$base_name.tar" -C "$path" "$base_name"
+    fi
+  fi
+done
+
+echo "Operation completed successfully."
