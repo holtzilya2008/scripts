290b0f6c193fd2c68715ef8910ece5a2c9db9342
Ilya Holtz <ilya.holtz@zoomd.com>
2023-07-31 17:55:30 +0300

    improoved scan_repos script

--- git-work/scan_repos.sh
+++ git-work/scan_repos.sh
@@ -15,11 +15,14 @@ total_keywords=${#keywords[@]}
 
 # iterate over the list of repositories
 for repo in "${repos[@]}"; do
-    # clone the repo
-    git clone --depth 1 "$repo"
 
     # extract the repo name from the repo URL
     repo_name=$(basename "$repo" .git)
+
+    echo "Working on Repository: $repo_name"
+
+    # clone the repo
+    git clone --depth 1 "$repo"
 
     # change to the cloned repository directory
     cd "$repo_name" || exit
@@ -27,16 +30,14 @@ for repo in "${repos[@]}"; do
     # iterate over the list of keywords
     for i in "${!keywords[@]}"; do
         keyword=${keywords[$i]}
-        echo "Scanning for $keyword in $repo_name"
 
         # calculate the progress as a percentage
         progress=$(( (i+1) * 100 / total_keywords ))
 
         # print the progress on the same line, clearing the previous output
-        echo -ne "Scanning repo $progress% \r"
-
+        echo -ne "Scanning repo $repo_name $progress% \r"
         # search for the keyword in the source code
-        grep -rnw '.' -e "$keyword" >> "../$repo_name.log"
+        grep -rn '.' -e "$keyword" >> "../$repo_name.log"
     done
 
     # print a newline to separate the outputs from different repositories
@@ -47,6 +48,10 @@ for repo in "${repos[@]}"; do
 
     # if the repo directory exists, delete it
     if [ -d "$repo_name" ]; then
+        echo "Cleaning up ..."
         rm -rf "$repo_name"
     fi
+
+    echo
+    echo
 done
